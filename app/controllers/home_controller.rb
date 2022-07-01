228f5052cb37d8efab1aa998e182468afd5a54e2
Graham Lewis <44037625+gclssvglx@users.noreply.github.com>
2022-07-01 16:46:05 +0100

    Refactor to add/remove runs

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,10 +1,18 @@
 class HomeController < ApplicationController
   def index
+    if params[:env] && params[:runs]
+      @environment = params[:env]
+      @runs = params[:runs]
+    end
+
     @interaction_types = ApplicationController.helpers.interaction_types
-    @environments = %w[integration] # just add 'staging'
+    @environments = %w[integration] # just add 'staging' when the time comes!
   end
 
   def fake
-    FakerJob.perform_now(params)
+    params[:runs].each do |key, value|
+      value[:environment] = params[:environment]
+      FakerJob.perform_now value
+    end
   end
 end
