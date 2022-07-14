b5b60ef819cb09f75315bacb5591c4618cab2eee
Rosa Fox <rosa.fox@digital.cabinet-office.gov.uk>
2022-07-14 17:17:09 +0100

    Add GoogleTagManager model
    This will be a parent class, used for code that can be reused across its
    future children.
    
    It is intialised with the inputs for GTM. It will be responsible for
    interfacing between these inputs and GTM.

---
+++ app/models/google_tag_manager.rb
@@ -0,0 +1,29 @@
+require "webdrivers"
+require "yaml"
+require "fileutils"
+
+class GoogleTagManager
+  attr_reader :options, :interactions, :driver, :output_file
+
+  def initialize(options)
+    @options = options
+    @interactions = interaction_data
+    @capabilities = Selenium::WebDriver::Remote::Capabilities.chrome(
+      "goog:chromeOptions": { args: %w(headless disable-gpu) }
+    )
+    @driver = Selenium::WebDriver.for :chrome, capabilities: @capabilities
+    FileUtils.mkdir_p "log"
+  end
+
+  def environment
+    options[:environment]
+  end
+
+  def interaction_type
+    options[:interaction_type]
+  end
+
+  def iterations
+     options[:iterations] ||= 1
+  end
+end
