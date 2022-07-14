9e0f81b0e660a05ff6e1f81392933e092a5e17a3
Rosa Fox <rosa.fox@digital.cabinet-office.gov.uk>
2022-07-14 17:38:29 +0100

    Add CreateEvent model
    Outputs events to a text file. Refactored from an original implementation
    in [the gtm-utils repo](https://github.com/gclssvglx/gtm-utils/blob/main/google_tag_manager.rb).
    
    Will be updated to also be used by the FakerJob in a future commit.

---
+++ app/models/create_events.rb
@@ -0,0 +1,61 @@
+class CreateEvents < GoogleTagManager
+  def initialize(options)
+    super
+  end
+
+  def run
+    events_output_to_file
+  end
+
+  private
+
+  def create_events
+    begin
+      if interaction_type == "pageviews"
+        find_interaction_urls.each do |url|
+          iterations.to_i.times do
+            get_url(url)
+            output_event_data
+          end
+        end
+      else
+        find_interaction_urls.each do |url|
+          iterations.to_i.times do
+            get_url(url)
+            clickables.each do |clickable|
+              clickable.click
+              output_event_data
+            end
+          end
+        end
+      end
+    ensure
+      driver.quit
+    end
+  end
+
+  def output_event_data
+    @output_file.puts get_event
+    @output_file.puts "\n"
+  end
+
+  def events_output_to_file
+    begin
+      @output_file = File.open("log/#{Time.now.strftime('%Y-%m-%d-%H-%M-%S')}.log", "w")
+      create_events
+    ensure
+      @output_file.close
+    end
+  end
+
+  def get_event
+    events.each do |event|
+      if interaction_type == "pageviews"
+        return event if event["event"] == "config_ready"
+      else
+        return event if event["event"] == "analytics"
+      end
+    end
+    { error: "Unknown interaction type #{interaction_type}" }.to_json
+  end
+end
