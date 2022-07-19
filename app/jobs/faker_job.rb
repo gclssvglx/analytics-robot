ad78890baaa37f9246bb1f5edd5fe27a69f64b1c
Graham Lewis <44037625+gclssvglx@users.noreply.github.com>
2022-07-19 16:18:15 +0100

    Merge with main branch

--- app/jobs/faker_job.rb
+++ app/jobs/faker_job.rb
@@ -5,52 +5,7 @@ class FakerJob < ApplicationJob
   queue_as :default
 
   def perform(options)
-    # faker_job = FakeEvents.new(options)
-    # faker_job.run
-    capabilities = Selenium::WebDriver::Remote::Capabilities.chrome(
-      'goog:chromeOptions': { args: %w(headless disable-gpu) }
-    )
-    driver = Selenium::WebDriver.for :chrome, capabilities: capabilities
-
-    begin
-      interaction_type = options[:interaction_type]
-      interactions = ApplicationController.helpers.interactions[interaction_type]
-
-      options[:iterations].to_i.times do
-        if interaction_type == "pageviews"
-          interactions["urls"].each do |url|
-            driver.get ApplicationController.helpers.environment_url(url, options[:environment])
-            ActionCable.server.broadcast 'faker_channel', find_event(driver, interaction_type)
-          end
-        else
-          interactions["urls"].each do |url|
-            driver.get ApplicationController.helpers.environment_url(url, options[:environment])
-            clickables = driver.find_elements(class: interactions["class"])
-            clickables.each do |clickable|
-              clickable.click
-              ActionCable.server.broadcast 'faker_channel', find_event(driver, interaction_type)
-            end
-          end
-        end
-      end
-    ensure
-      driver.quit
-    end
-  end
-
-  private
-
-  def find_event(driver, interaction_type)
-    events = driver.execute_script("return dataLayer")
-
-    event = nil
-    if interaction_type == "pageviews"
-      events.each do |e|
-        event = e if e["event"] == "config_ready"
-      end
-    else
-      event = events.last
-    end
-    event.to_json
+    faker_job = FakeEvents.new(options)
+    faker_job.run
   end
 end
--- app/models/fake_events.rb
+++ app/models/fake_events.rb
@@ -40,7 +40,7 @@ class FakeEvents < GtmEventGenerator
 
   def output_event_data
     if display == "browser"
-      ActionCable.server.broadcast 'faker_channel', get_event.to_json
+      ActionCable.server.broadcast 'faker_channel', get_event
     else
       @output_file.puts get_event
       @output_file.puts "\n"
@@ -57,13 +57,14 @@ class FakeEvents < GtmEventGenerator
   end
 
   def get_event
-    events.each do |event|
-      if interaction_type == "pageviews"
-        return event if event["event"] == "config_ready"
-      else
-        return event if event["event"] == "analytics"
+    event = nil
+    if interaction_type == "pageviews"
+      events.each do |e|
+        event = e if e["event"] == "config_ready"
       end
+    else
+      event = events.last
     end
-    { error: "Unknown interaction type #{interaction_type}" }.to_json
+    event.to_json
   end
 end
