1fbbae87e39e1a889cf2a7c6f46b0ef8982fd614
Graham Lewis <44037625+gclssvglx@users.noreply.github.com>
2022-08-01 10:47:59 +0100

    Add test_events for page_views

--- app/models/test_events.rb
+++ app/models/test_events.rb
@@ -14,11 +14,15 @@ class TestEvents < GtmEventGenerator
       find_interaction_urls.each do |url|
         iterations.to_i.times do
           get_url(url)
-          clickables.each do |clickable|
-            if interaction_type == "tabs"
-              test_tab_events(clickable)
-            elsif interaction_type == "accordions"
-              test_accordion_events(clickable)
+          if interaction_type == "pageviews"
+            test_pageview_events if interaction_type == "pageviews"
+          elsif %w[tabs accordions].include?(interaction_type)
+            clickables.each do |clickable|
+              if interaction_type == "tabs"
+                test_tab_events(clickable)
+              elsif interaction_type == "accordions"
+                test_accordion_events(clickable)
+              end
             end
           end
         end
@@ -28,22 +32,31 @@ class TestEvents < GtmEventGenerator
     end
   end
 
-  def test_result(expected_event)
-    last_event = events.last.except("gtm.uniqueEventId")
-    print last_event.eql?(expected_event) ? "😀" : "\n🤮 : #{diff_events(last_event, expected_event)}\n"
+  def test_result(actual_event, expected_event)
+    event = actual_event.except("gtm.uniqueEventId")
+    print event.eql?(expected_event) ? "😀" : "\n🤮 : #{diff_events(event, expected_event)}\n"
   end
 
   def test_tab_events(tab)
     tab.click
-    expected_event = create_event(events.last["event_data"])
-    test_result(expected_event)
+    expected_event = create_event_data(events.last["event_data"])
+    test_result(events.last, expected_event)
   end
 
   def test_accordion_events(accordion)
     %w[opened closed].each do |state|
       accordion.click
-      expected_event = create_event(events.last["event_data"])
-      test_result(expected_event)
+      expected_event = create_event_data(events.last["event_data"])
+      test_result(events.last, expected_event)
+    end
+  end
+
+  def test_pageview_events
+    events.reverse.each do |event|
+      if event["event"] == "page_view"
+        expected_event = create_page_view_data(event["page_view"])
+        test_result(event, expected_event)
+      end
     end
   end
 
@@ -56,7 +69,7 @@ class TestEvents < GtmEventGenerator
     end
   end
 
-  def create_event(data_attributes)
+  def create_event_data(data_attributes)
     {
       "event" => "event_data",
       "event_data" => {
@@ -69,6 +82,40 @@ class TestEvents < GtmEventGenerator
         "text" => data_attributes["text"],
         "type" => data_attributes["type"],
         "url" => data_attributes["url"],
+      }
+    }
+  end
+
+  def create_page_view_data(data_attributes)
+    {
+      "event" => "page_view",
+      "page_view" => {
+        "content_id" => data_attributes["content_id"],
+        "document_type" => data_attributes["document_type"],
+        "first_published_at" => data_attributes["first_published_at"],
+        "history" => data_attributes["history"],
+        "language" => data_attributes["language"],
+        "location" => data_attributes["location"],
+        "organisations" => data_attributes["organisations"],
+        "political_status" => data_attributes["political_status"],
+        "primary_publishing_organisation" => data_attributes["primary_publishing_organisation"],
+        "public_updated_at" => data_attributes["public_updated_at"],
+        "publishing_app" => data_attributes["publishing_app"],
+        "publishing_government" => data_attributes["publishing_government"],
+        "referrer" => data_attributes["referrer"],
+        "rendering_app" => data_attributes["rendering_app"],
+        "schema_name" => data_attributes["schema_name"],
+        "section" => data_attributes["section"],
+        "status_code" => data_attributes["status_code"],
+        "taxon_id" => data_attributes["taxon_id"],
+        "taxon_ids" => data_attributes["taxon_ids"],
+        "taxon_slug" => data_attributes["taxon_slug"],
+        "taxon_slugs" => data_attributes["taxon_slugs"],
+        "themes" => data_attributes["themes"],
+        "title" => data_attributes["title"],
+        "updated_at" => data_attributes["updated_at"],
+        "withdrawn" => data_attributes["withdrawn"],
+        "world_locations" => data_attributes["world_locations"],
       }
     }
   end
